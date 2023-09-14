d249971250433c67c5cdc92fbdfac677c9cb3c52
Joseph Lurvey <joeylurvey@gmail.com>
2023-09-14 19:34:59 -0400

    Completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        f_list = []
+    elif length == 1:
+        f_list = [0]
+    elif length > 1:
+        f_list = [0, 1]
+
+    while len(f_list) < length:
+        next_number = f_list[-1] + f_list[-2]
+        f_list.append(next_number)
+
+    print(f_list)
